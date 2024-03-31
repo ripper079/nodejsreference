c1150b4b336bc3e64d66a8ca3af9137c2f36e878
daniel <daniel@oikarainen.se>
2024-03-31 08:53:39 +0200

    Create file - Demo the global object

---
+++ js/global_object.js
@@ -0,0 +1,5 @@
+//Global object
+//Global object is an object that provides access to global variables and functions across all modules in a Node.js application. It has functions and properties attached to it (similar to window object in web browser). Its named
+// global
+
+console.log(global);
